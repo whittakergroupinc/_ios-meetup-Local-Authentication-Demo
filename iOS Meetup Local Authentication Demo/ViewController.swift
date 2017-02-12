4becc72f26493b70e286dbbf410ee278cc71ce86
David Whittaker <david@whittakergroupinc.com>
2017-02-12 12:40:03 -0500

    Initial Commit

---
+++ iOS Meetup Local Authentication Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Meetup Local Authentication Demo
+//
+//  Created by David Whittaker on 2/12/17.
+//  Copyright © 2017 The Whittaker Group, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOS Meetup Local Authentication DemoTests/iOS_Meetup_Local_Authentication_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_Meetup_Local_Authentication_DemoTests.swift
+//  iOS Meetup Local Authentication DemoTests
+//
+//  Created by David Whittaker on 2/12/17.
+//  Copyright © 2017 The Whittaker Group, Inc. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Meetup_Local_Authentication_Demo
+
+class iOS_Meetup_Local_Authentication_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
